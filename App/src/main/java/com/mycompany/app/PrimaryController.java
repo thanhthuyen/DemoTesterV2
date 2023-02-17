41a998f7b82f9e31402b122cc8cb9af74872e8e6
ThanhThuyen <2051050488thuyen@ou.edu.vn>
2023-02-17 17:04:18 +0700

    fs

--- App/src/main/java/com/mycompany/app/PrimaryController.java
+++ App/src/main/java/com/mycompany/app/PrimaryController.java
@@ -7,6 +7,11 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        int a=9;
+        if(a>0)
+            {
+                App.setRoot("secondary");
+            }
+        
     }
 }
